7670dcde3cb32a0f4e793261ea3c55836224ec1c
B Kleinen <kleinen@infrastructure.de>
2013-12-19 08:53:26 +0100

    split up chapter 3

---
+++ chapter_3_gear_wrapper.rb
@@ -0,0 +1,61 @@
+# wrap the interface to protect yourself from changes
+module GearWrapper
+  def self.gear(args)
+    SomeFramework::Gear.new(args[:chainring],
+                            args[:cog],
+                            args[:wheel])
+  end
+end
+
+# Now you can create a new Gear using an arguments hash.
+GearWrapper.gear(
+  :chainring => 52,
+  :cog       => 11,
+  :wheel     => Wheel.new(26, 1.5)).gear_inches
+
+############## Page ?? ##############
+# This is the complete code for example above
+module SomeFramework
+  class Gear
+    attr_reader :chainring, :cog, :wheel
+    def initialize(chainring, cog, wheel)
+      @chainring = chainring
+      @cog       = cog
+      @wheel     = wheel
+    end
+
+    def gear_inches
+      ratio * wheel.diameter
+    end
+
+    def ratio
+      chainring / cog.to_f
+    end
+  end
+end
+
+class Wheel
+  attr_reader :rim, :tire
+  def initialize(rim, tire)
+    @rim       = rim
+    @tire      = tire
+  end
+
+  def diameter
+    rim + (tire * 2)
+  end
+end
+
+module GearWrapper
+  def self.gear(args)
+    SomeFramework::Gear.new(args[:chainring],
+                            args[:cog],
+                            args[:wheel])
+  end
+end
+
+GearWrapper.gear(
+  :chainring => 52,
+  :cog       => 11,
+  :wheel     => Wheel.new(26, 1.5)).gear_inches
+
---
+++ chapter_3_p41.rb
@@ -0,0 +1,17 @@
+############## Page 41 ##############
+class Gear
+  attr_reader :chainring, :cog, :wheel
+  def initialize(chainring, cog, wheel)
+    @chainring = chainring
+    @cog       = cog
+    @wheel     = wheel
+  end
+
+  def gear_inches
+    ratio * wheel.diameter
+  end
+# ...
+end
+
+# Gear expects a 'Duck' that knows 'diameter'
+Gear.new(52, 11, Wheel.new(26, 1.5)).gear_inches
